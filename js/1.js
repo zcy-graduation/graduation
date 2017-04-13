224609d70751824d3a132be0af8b8edeed605031
Erossssss <you@example.com>
2017-04-13 17:45:04 +0800

    1

--- js/1.js
+++ js/1.js
@@ -1,47 +1,74 @@
 /**
  * Created by Administrator on 2017/4/12.
  */
-$.fn.extend({
-    mouseWheel:function (up,down) {
-        this.each(function (index,val) {
-            val.addEventListener('DOMMouseScroll',fun,false);
-            val.addEventListener('mousewheel',fun,false);
-            function fun(e) {
-                let nub=e.wheelDelta||e.detail;
-                e.preventDefault();
-                if(nub==120||nub==-3){
-                    down.call(val)
-                }
-                if(nub==-120||nub==3){
-                    up.call(val)
-                }
-            }
-        });
-        return this
-    }
-});
-let aa=40;
 $(document).ready(function () {
-    $('body').mouseWheel(function () {
-        aa=aa-20;
-        console.log(1);
-        $('.box').css('transform',`translateZ(${aa}px)`)
-    },function () {
-        aa=aa+20;
-        console.log(2);
-        $('.box').css('transform',`translateZ(${aa}px)`)
-    });
-
-    document.onmousedown=function (e) {
-        let x=e.clientX;
-        let y=e.clientY;
-        document.onmousemove=function (e) {
-            let x1=e.clientX;
-            let y1=e.clientY;
-            let xx=(x1-x)+`px`;
-            let yy=(y1-y)+`px`;
-            $('body').css('perspective-origin',{xx,yy})
-            // console.log($('body').css('perspective-origin',"xx yy"))
+    if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
+    let container, stats;
+    let camera, controls, scene, renderer;
+    let cross;
+    init();
+    animate();
+    function init() {
+        camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
+        camera.position.z = 500;
+        controls = new THREE.TrackballControls( camera );
+        controls.rotateSpeed = 1.0;
+        controls.zoomSpeed = 1.2;
+        controls.panSpeed = 0.8;
+        controls.noZoom = false;
+        controls.noPan = false;
+        controls.staticMoving = true;
+        controls.dynamicDampingFactor = 0.3;
+        controls.keys = [ 65, 83, 68 ];
+        controls.addEventListener( 'change', render );
+        // world
+        scene = new THREE.Scene();
+        scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
+        let geometry = new THREE.CylinderGeometry( 0, 10, 30, 4, 1 );
+        let material =  new THREE.MeshPhongMaterial( { color:0xffffff, shading: THREE.FlatShading } );
+        for ( let i = 0; i < 500; i ++ ) {
+            let mesh = new THREE.Mesh( geometry, material );
+            mesh.position.x = ( Math.random() - 0.5 ) * 1000;
+            mesh.position.y = ( Math.random() - 0.5 ) * 1000;
+            mesh.position.z = ( Math.random() - 0.5 ) * 1000;
+            mesh.updateMatrix();
+            mesh.matrixAutoUpdate = false;
+            scene.add( mesh );
         }
+        // lights
+        light = new THREE.DirectionalLight( 0xffffff );
+        light.position.set( 1, 1, 1 );
+        scene.add( light );
+        light = new THREE.DirectionalLight( 0x002288 );
+        light.position.set( -1, -1, -1 );
+        scene.add( light );
+        light = new THREE.AmbientLight( 0x222222 );
+        scene.add( light );
+        // renderer
+        renderer = new THREE.WebGLRenderer( { antialias: false } );
+        renderer.setClearColor( scene.fog.color );
+        renderer.setPixelRatio( window.devicePixelRatio );
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        container = document.getElementById( 'container' );
+        container.appendChild( renderer.domElement );
+
+
+        window.addEventListener( 'resize', onWindowResize, false );
+        //
+        render();
+    }
+    function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        controls.handleResize();
+        render();
+    }
+    function animate() {
+        requestAnimationFrame( animate );
+        controls.update();
+    }
+    function render() {
+        renderer.render( scene, camera );
     }
 });
