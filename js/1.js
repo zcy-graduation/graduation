e268ea458c5dcdb9d7d704b76368b60d1a60ff01
Erossssss <you@example.com>
2017-04-17 17:43:52 +0800

    1

--- js/1.js
+++ js/1.js
@@ -6,34 +6,127 @@ $(document).ready(function () {
     let container, stats;
     let camera, controls, scene, renderer;
     let cross;
+    let raycaster;
+    let mouse;
     init();
     animate();
     function init() {
         camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
-        camera.position.z = 500;
+        camera.position.z =500;
         controls = new THREE.TrackballControls( camera );
-        controls.rotateSpeed = 1.0;
-        controls.zoomSpeed = 1.2;
-        controls.panSpeed = 0.8;
-        controls.noZoom = false;
-        controls.noPan = false;
-        controls.staticMoving = true;
-        controls.dynamicDampingFactor = 0.3;
+        controls.rotateSpeed = 1.5; //旋转速度
+        controls.zoomSpeed = 1.2;   //变焦速度
+        controls.panSpeed = 0.8;    //平移速度
+        controls.noZoom = false;    //是否不变焦
+        controls.noPan = false;     //是否不平移（点击鼠标右键滑动）
+        controls.staticMoving = true;   //惯性
+        controls.dynamicDampingFactor = 0.3;    //动态阻尼系数（灵敏度）
         controls.keys = [ 65, 83, 68 ];
         controls.addEventListener( 'change', render );
         // world
         scene = new THREE.Scene();
-        scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
-        let geometry = new THREE.CylinderGeometry( 0, 10, 30, 4, 1 );
-        let material =  new THREE.MeshPhongMaterial( { color:0xffffff, shading: THREE.FlatShading } );
-        for ( let i = 0; i < 500; i ++ ) {
-            let mesh = new THREE.Mesh( geometry, material );
-            mesh.position.x = ( Math.random() - 0.5 ) * 1000;
-            mesh.position.y = ( Math.random() - 0.5 ) * 1000;
-            mesh.position.z = ( Math.random() - 0.5 ) * 1000;
+        scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );   //创建雾效  geometry几何
+        let geometry=new THREE.PlaneGeometry(150,100);  //平面大小
+        let main=$('#container');
+        console.log(main);
+        let material=[];    //创建一个空数组，放图片
+        for ( let i = 0; i < 17; i ++ ) {
+            let texture=THREE.ImageUtils.loadTexture(`../img2/${i}.jpg`);
+            material.push(new THREE.MeshBasicMaterial({map:texture}));
+            let mesh = new THREE.Mesh( geometry, material[i] );
+            mesh.position.x = ( Math.random() - 0.5 ) * 668;
+            mesh.position.y = ( Math.random() - 0.5 ) * 300;
+            mesh.position.z = ( Math.random() - 0.5 ) * 700;
+            $('mesh').css('cursor','pointer');
+            switch (i){
+                case 0:
+                    mesh.userData={
+                        URL:"https://www.baidu.com/"
+                    };break;
+                case 1:
+                    mesh.userData={
+                        URL:"https://www.baidu.com/"
+                    };break;
+                case 2:
+                    mesh.userData={
+                        URL:"https://www.baidu.com/"
+                    };break;
+                case 3:
+                    mesh.userData={
+                        URL:"https://www.baidu.com/"
+                    };break;
+                case 4:
+                    mesh.userData={
+                        URL:"https://www.baidu.com/"
+                    };break;
+                case 5:
+                    mesh.userData={
+                        URL:"https://www.baidu.com/"
+                    };break;
+                case 6:
+                    mesh.userData={
+                        URL:"https://www.baidu.com/"
+                    };break;
+                case 7:
+                    mesh.userData={
+                        URL:"https://www.baidu.com/"
+                    };break;
+                case 8:
+                    mesh.userData={
+                        URL:"https://www.baidu.com/"
+                    };break;
+                case 9:
+                    mesh.userData={
+                        URL:"https://www.baidu.com/"
+                    };break;
+
+            }
             mesh.updateMatrix();
             mesh.matrixAutoUpdate = false;
             scene.add( mesh );
+        }
+
+        let sides=[
+            {
+                url:'../img/posx.jpg',
+                position:[-512,0,0],
+                rotation:[0,Math.PI/2,0]
+            },
+            {
+                url:'../img/negx.jpg',
+                position:[512,0,0],
+                rotation:[0,-Math.PI/2,0]
+            },
+            {
+                url:'../img/posy.jpg',
+                position:[0,512,0],
+                rotation:[Math.PI/2,0,Math.PI]
+            },
+            {
+                url:'../img/negy.jpg',
+                position:[0,-512,0],
+                rotation:[-Math.PI/2,0,Math.PI]
+            },
+            {
+                url:'../img/posz.jpg',
+                position:[0,0,512],
+                rotation:[0,Math.PI,0]
+            },
+            {
+                url:'../img/negz.jpg',
+                position:[0,0,-512],
+                rotation:[0,0,0]
+            },
+        ];
+        for(let i=0;i<sides.length;i++){
+            let side = sides[ i ];
+            let element = document.createElement( 'img' );
+            element.width = 1026; // 2 pixels extra to close the gap.
+            element.src = side.url;
+            let object = new THREE.CSS3DObject( element );
+            object.position.fromArray( side.position );
+            object.rotation.fromArray( side.rotation );
+            scene.add( object );
         }
         // lights
         light = new THREE.DirectionalLight( 0xffffff );
@@ -45,17 +138,29 @@ $(document).ready(function () {
         light = new THREE.AmbientLight( 0x222222 );
         scene.add( light );
         // renderer
-        renderer = new THREE.WebGLRenderer( { antialias: false } );
-        renderer.setClearColor( scene.fog.color );
+
+        raycaster=new THREE.Raycaster();  //创建射线，以确定是否与3D对象相交
+        mouse=new THREE.Vector2();  //新建一个Vector2对象保存鼠标位置信息，监听鼠标移动事件
+
+        renderer = new THREE.WebGLRenderer( { antialias: false } ); //生成渲染器对象，锯齿效果false
+        renderer.setClearColor( scene.fog.color ); //设置渲染器清除色
         renderer.setPixelRatio( window.devicePixelRatio );
         renderer.setSize( window.innerWidth, window.innerHeight );
         container = document.getElementById( 'container' );
         container.appendChild( renderer.domElement );
 
 
-        window.addEventListener( 'resize', onWindowResize, false );
+
         //
+
+
+
+
+        document.addEventListener( 'mousedown', onDocumentMouseDown, false );
+        document.addEventListener( 'touchstart', onDocumentTouchStart, false );
+        window.addEventListener( 'resize', onWindowResize, false );
         render();
+
     }
     function onWindowResize() {
         camera.aspect = window.innerWidth / window.innerHeight;
@@ -64,11 +169,33 @@ $(document).ready(function () {
         controls.handleResize();
         render();
     }
+
+    function onDocumentTouchStart( event ) {
+        event.preventDefault();
+        event.clientX = event.touches[0].clientX;
+        event.clientY = event.touches[0].clientY;
+        onDocumentMouseDown( event );
+    }
+    function onDocumentMouseDown( event ) {
+        event.preventDefault();
+        mouse.x = ( event.clientX / renderer.domElement.clientWidth ) * 2 - 1;
+        mouse.y = - ( event.clientY / renderer.domElement.clientHeight ) * 2 + 1;
+        raycaster.setFromCamera( mouse, camera );
+
+        let intersects = raycaster.intersectObjects(scene.children);    //定义射线拾取物体
+
+        if ( intersects.length > 0 ) {
+            location.assign(intersects[0].object.userData.URL); //当射中打开链接
+        }
+    }
+
     function animate() {
         requestAnimationFrame( animate );
         controls.update();
     }
+
     function render() {
+
         renderer.render( scene, camera );
     }
 });
--- js/CSS3DRenderer.js
+++ js/CSS3DRenderer.js
@@ -10,7 +10,7 @@ THREE.CSS3DObject = function ( element ) {
 	this.element = element;
 	this.element.style.position = 'absolute';
 
-	this.addEventListener( 'removed', function () {
+	this.addEventListener( 'removed', function ( event ) {
 
 		if ( this.element.parentNode !== null ) {
 
@@ -53,21 +53,25 @@ THREE.CSS3DRenderer = function () {
 	var domElement = document.createElement( 'div' );
 	domElement.style.overflow = 'hidden';
 
+	domElement.style.WebkitTransformStyle = 'preserve-3d';
+	domElement.style.MozTransformStyle = 'preserve-3d';
+	domElement.style.oTransformStyle = 'preserve-3d';
+	domElement.style.transformStyle = 'preserve-3d';
+
 	this.domElement = domElement;
 
 	var cameraElement = document.createElement( 'div' );
 
 	cameraElement.style.WebkitTransformStyle = 'preserve-3d';
 	cameraElement.style.MozTransformStyle = 'preserve-3d';
+	cameraElement.style.oTransformStyle = 'preserve-3d';
 	cameraElement.style.transformStyle = 'preserve-3d';
 
 	domElement.appendChild( cameraElement );
 
-	var isIE = /Trident/i.test( navigator.userAgent );
-
 	this.setClearColor = function () {};
 
-	this.getSize = function () {
+	this.getSize = function() {
 
 		return {
 			width: _width,
@@ -80,6 +84,7 @@ THREE.CSS3DRenderer = function () {
 
 		_width = width;
 		_height = height;
+
 		_widthHalf = _width / 2;
 		_heightHalf = _height / 2;
 
@@ -91,13 +96,13 @@ THREE.CSS3DRenderer = function () {
 
 	};
 
-	function epsilon( value ) {
+	var epsilon = function ( value ) {
 
-		return Math.abs( value ) < 1e-10 ? 0 : value;
+		return Math.abs( value ) < Number.EPSILON ? 0 : value;
 
-	}
+	};
 
-	function getCameraCSSMatrix( matrix ) {
+	var getCameraCSSMatrix = function ( matrix ) {
 
 		var elements = matrix.elements;
 
@@ -120,12 +125,13 @@ THREE.CSS3DRenderer = function () {
 			epsilon( elements[ 15 ] ) +
 		')';
 
-	}
+	};
 
-	function getObjectCSSMatrix( matrix, cameraCSSMatrix ) {
+	var getObjectCSSMatrix = function ( matrix ) {
 
 		var elements = matrix.elements;
-		var matrix3d = 'matrix3d(' +
+
+		return 'translate3d(-50%,-50%,0) matrix3d(' +
 			epsilon( elements[ 0 ] ) + ',' +
 			epsilon( elements[ 1 ] ) + ',' +
 			epsilon( elements[ 2 ] ) + ',' +
@@ -144,20 +150,9 @@ THREE.CSS3DRenderer = function () {
 			epsilon( elements[ 15 ] ) +
 		')';
 
-		if ( isIE ) {
+	};
 
-			return 'translate(-50%,-50%)' +
-				'translate(' + _widthHalf + 'px,' + _heightHalf + 'px)' +
-				cameraCSSMatrix +
-				matrix3d;
-
-		}
-
-		return 'translate(-50%,-50%)' + matrix3d;
-
-	}
-
-	function renderObject( object, camera, cameraCSSMatrix ) {
+	var renderObject = function ( object, camera ) {
 
 		if ( object instanceof THREE.CSS3DObject ) {
 
@@ -177,30 +172,25 @@ THREE.CSS3DRenderer = function () {
 				matrix.elements[ 11 ] = 0;
 				matrix.elements[ 15 ] = 1;
 
-				style = getObjectCSSMatrix( matrix, cameraCSSMatrix );
+				style = getObjectCSSMatrix( matrix );
 
 			} else {
 
-				style = getObjectCSSMatrix( object.matrixWorld, cameraCSSMatrix );
+				style = getObjectCSSMatrix( object.matrixWorld );
 
 			}
 
 			var element = object.element;
-			var cachedStyle = cache.objects[ object.id ] && cache.objects[ object.id ].style;
+			var cachedStyle = cache.objects[ object.id ];
 
 			if ( cachedStyle === undefined || cachedStyle !== style ) {
 
 				element.style.WebkitTransform = style;
 				element.style.MozTransform = style;
+				element.style.oTransform = style;
 				element.style.transform = style;
 
-				cache.objects[ object.id ] = { style: style };
-
-				if ( isIE ) {
-
-					cache.objects[ object.id ].distanceToCameraSquared = getDistanceToSquared( camera, object );
-
-				}
+				cache.objects[ object.id ] = style;
 
 			}
 
@@ -214,50 +204,11 @@ THREE.CSS3DRenderer = function () {
 
 		for ( var i = 0, l = object.children.length; i < l; i ++ ) {
 
-			renderObject( object.children[ i ], camera, cameraCSSMatrix );
+			renderObject( object.children[ i ], camera );
 
 		}
 
-	}
-
-	var getDistanceToSquared = function () {
-
-		var a = new THREE.Vector3();
-		var b = new THREE.Vector3();
-
-		return function ( object1, object2 ) {
-
-			a.setFromMatrixPosition( object1.matrixWorld );
-			b.setFromMatrixPosition( object2.matrixWorld );
-
-			return a.distanceToSquared( b );
-
-		};
-
-	}();
-
-	function zOrder( scene ) {
-
-		var order = Object.keys( cache.objects ).sort( function ( a, b ) {
-
-			return cache.objects[ a ].distanceToCameraSquared - cache.objects[ b ].distanceToCameraSquared;
-
-		} );
-		var zMax = order.length;
-
-		scene.traverse( function ( object ) {
-
-			var index = order.indexOf( object.id + '' );
-
-			if ( index !== - 1 ) {
-
-				object.element.style.zIndex = zMax - index;
-
-			}
-
-		} );
-
-	}
+	};
 
 	this.render = function ( scene, camera ) {
 
@@ -265,9 +216,10 @@ THREE.CSS3DRenderer = function () {
 
 		if ( cache.camera.fov !== fov ) {
 
-			domElement.style.WebkitPerspective = fov + 'px';
-			domElement.style.MozPerspective = fov + 'px';
-			domElement.style.perspective = fov + 'px';
+			domElement.style.WebkitPerspective = fov + "px";
+			domElement.style.MozPerspective = fov + "px";
+			domElement.style.oPerspective = fov + "px";
+			domElement.style.perspective = fov + "px";
 
 			cache.camera.fov = fov;
 
@@ -279,33 +231,21 @@ THREE.CSS3DRenderer = function () {
 
 		camera.matrixWorldInverse.getInverse( camera.matrixWorld );
 
-		var cameraCSSMatrix = 'translateZ(' + fov + 'px)' +
-			getCameraCSSMatrix( camera.matrixWorldInverse );
+		var style = "translate3d(0,0," + fov + "px)" + getCameraCSSMatrix( camera.matrixWorldInverse ) +
+			" translate3d(" + _widthHalf + "px," + _heightHalf + "px, 0)";
 
-		var style = cameraCSSMatrix +
-			'translate(' + _widthHalf + 'px,' + _heightHalf + 'px)';
-
-		if ( cache.camera.style !== style && ! isIE ) {
+		if ( cache.camera.style !== style ) {
 
 			cameraElement.style.WebkitTransform = style;
 			cameraElement.style.MozTransform = style;
+			cameraElement.style.oTransform = style;
 			cameraElement.style.transform = style;
 
 			cache.camera.style = style;
 
 		}
 
-		renderObject( scene, camera, cameraCSSMatrix );
-
-		if ( isIE ) {
-
-			// IE10 and 11 does not support 'preserve-3d'.
-			// Thus, z-order in 3D will not work.
-			// We have to calc z-order manually and set CSS z-index for IE.
-			// FYI: z-index can't handle object intersection
-			zOrder( scene );
-
-		}
+		renderObject( scene, camera );
 
 	};
 
